6a1cca4573e6d51ac7a0ae2c8aeeb001bb126294
Manuel <mmonto952@gmail.com>
2021-12-10 04:36:02 -0500

    Modifiqué el dashboard ligeramente

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,8 +4,6 @@ from dash import dcc
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
-import plotly.express as px
-import pandas as pd
 import numpy as np
 from scipy.stats import mode
 
@@ -13,19 +11,16 @@ import base64
 import librosa
 import joblib
 import io
+import os
 
 from helpers import extract_features, WINDOW_SIZE, BEST_MODEL, MUSIC_GENRES
+
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-styles = {
-    'pre': {
-        # 'border': 'thin lightgrey solid',
-        # 'overflowX': 'scroll'
-    }
-}
+scaler = joblib.load('../data/models/knn_937_scaler.joblib')
 
 app.layout = html.Div([
     html.Div(
@@ -48,27 +43,6 @@ app.layout = html.Div([
 ])
 
 
-def parse_contents(content, filename):
-    content_type, content_string = content.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return
-
-
 @app.callback(Output('output-data-upload', 'children'),
               Input('upload-song', 'contents'),
               State('upload-song', 'filename'))
@@ -89,9 +63,9 @@ def update_output(content, filename):
         for i in range(int(len(signal) / n_points)):
             window = signal[i * n_points:(i + 1) * n_points]
             if len(window):
-                values = extract_features(window)
+                values = scaler.transform(np.array(extract_features(window)).reshape(1, -1))
                 model = joblib.load(BEST_MODEL)
-                predictions.append(model.predict(np.array(values).reshape(1, -1))[0])
+                predictions.append(model.predict(values)[0])
 
         return html.Div([
             f'El género de la canción que subiste es',
@@ -101,87 +75,8 @@ def update_output(content, filename):
     return html.Div('Debes subir un archivo .wav')
 
 
-# app.layout = html.Div([
-#     dcc.Graph(
-#         id='basic-interactions',
-#         figure=fig
-#     ),
-#
-#     html.Div(className='row', children=[
-#         html.Div([
-#             dcc.Markdown("""
-#                 **Hover Data**
-#
-#                 Mouse over values in the graph.
-#             """),
-#             html.Pre(id='hover-data', style=styles['pre'])
-#         ], className='three columns'),
-#
-#         html.Div([
-#             dcc.Markdown("""
-#                 **Click Data**
-#
-#                 Click on points in the graph.
-#             """),
-#             html.Pre(id='click-data', style=styles['pre']),
-#         ], className='three columns'),
-#
-#         html.Div([
-#             dcc.Markdown("""
-#                 **Selection Data**
-#
-#                 Choose the lasso or rectangle tool in the graph's menu
-#                 bar and then select points in the graph.
-#
-#                 Note that if `layout.clickmode = 'event+select'`, selection data also
-#                 accumulates (or un-accumulates) selected data if you hold down the shift
-#                 button while clicking.
-#             """),
-#             html.Pre(id='selected-data', style=styles['pre']),
-#         ], className='three columns'),
-#
-#         html.Div([
-#             dcc.Markdown("""
-#                 **Zoom and Relayout Data**
-#
-#                 Click and drag on the graph to zoom or click on the zoom
-#                 buttons in the graph's menu bar.
-#                 Clicking on legend items will also fire
-#                 this event.
-#             """),
-#             html.Pre(id='relayout-data', style=styles['pre']),
-#         ], className='three columns')
-#     ])
-# ])
-
-#
-# @app.callback(
-#     Output('hover-data', 'children'),
-#     Input('basic-interactions', 'hoverData'))
-# def display_hover_data(hoverData):
-#     return json.dumps(hoverData, indent=2)
-#
-#
-# @app.callback(
-#     Output('click-data', 'children'),
-#     Input('basic-interactions', 'clickData'))
-# def display_click_data(clickData):
-#     return json.dumps(clickData, indent=2)
-#
-#
-# @app.callback(
-#     Output('selected-data', 'children'),
-#     Input('basic-interactions', 'selectedData'))
-# def display_selected_data(selectedData):
-#     return json.dumps(selectedData, indent=2)
-#
-#
-# @app.callback(
-#     Output('relayout-data', 'children'),
-#     Input('basic-interactions', 'relayoutData'))
-# def display_relayout_data(relayoutData):
-#     return json.dumps(relayoutData, indent=2)
-
-
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    if os.environ.get('ENV') == 'development':
+        app.run_server(debug=True)
+    else:
+        app.run_server(host='0.0.0.0', port=80)

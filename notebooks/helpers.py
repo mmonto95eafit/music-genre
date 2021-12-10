e29aaa3aada1ad9e7f6ed7bc6f2c2bae4517c452
kevin-stiv <kevinstivm@gmail.com>
2021-12-10 11:33:29 -0500

    Se agrega la fase 1 de entrenamiento, y se modifica gelpers para que al extraer los datasets lo haga con el método robusto.

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -1,6 +1,6 @@
 # ToDo: Add functions that we consider that are important
 
-import librosa
+#import librosa
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
@@ -71,41 +71,42 @@ def get_datasets(data_path='../data/features_3_sec.csv'):
     df = pd.read_csv(data_path)
     df['label'] = df['label'].astype('category')
     df['length'] = pd.to_numeric(df['length'])
-
+    ##################
     df_x = df.drop(['filename', 'label', 'length'], axis=1)
     x = np.array(df_x)
     y = df['label'].cat.codes.values
-
-    df_x_cf = df_x.drop(['spectral_centroid_mean', 'spectral_bandwidth_mean', 'rolloff_mean'], axis=1)
+    #########################
+    df_x_cf = df_x.drop(
+        ['spectral_centroid_mean', 'spectral_bandwidth_mean', 'rolloff_mean'], axis=1)
     x_cf = np.array(df_x_cf)
     y_cf = y
+    #######################################
 
     scaler = MinMaxScaler()
-    x_norm = scaler.fit_transform(x)
-    data = df.iloc[:, 2:-1]
-    data_norm = scaler.fit_transform(data)
-    cov_h_norm = np.cov(x_norm.T)
-    cov_i_norm = cov_h_norm + 10 * np.eye(cov_h_norm.shape[0], cov_h_norm.shape[1])
-    mahalanobis_dis_i_norm = mahalanobis(x=x_norm, data=data_norm, cov=cov_i_norm)
+    x_scaled = scaler.fit_transform(
+        df.loc[:, 'chroma_stft_mean':'mfcc20_var'].values)
+    y = df['label'].cat.codes.values
 
-    # x_fn = x_norm[mahalanobis_dis_i_norm < 0.4]
-    # y_fn = y[mahalanobis_dis_i_norm < 0.4]
+    cov = pd.read_csv('../data/kur_covariance_scaled.csv', header=None).values
 
-    p25 = np.percentile(mahalanobis_dis_i_norm, 25)
-    p75 = np.percentile(mahalanobis_dis_i_norm, 75)
+    v = x_scaled - x_scaled.mean(axis=0)
+    mahalanobis_distances = np.diag(v @ np.linalg.inv(cov) @ v.T)
+    p25 = np.percentile(mahalanobis_distances, 25)
+    p75 = np.percentile(mahalanobis_distances, 75)
     threshold = p75 + 3 * (p75 - p25)
-    x_fn = x_norm[mahalanobis_dis_i_norm < threshold]
-    y_fn = y[mahalanobis_dis_i_norm < threshold]
+    x_filtered = x_scaled[mahalanobis_distances < threshold]
+    y_filtered = y[mahalanobis_distances < threshold]
 
     cols = df_x.columns
-    df_x_fn = pd.DataFrame(x_fn, columns=cols)
-    df_x_fn_cf = df_x_fn.drop(['spectral_centroid_mean', 'spectral_bandwidth_mean', 'rolloff_mean'], axis=1)
-    x_fn_cf = np.array(df_x_fn_cf)
-    y_fn_cf = y_fn
+    df_x_filtered = pd.DataFrame(x_filtered, columns=cols)
+    df_x_filtered_cf = df_x_filtered.drop(
+        ['spectral_centroid_mean', 'spectral_bandwidth_mean', 'rolloff_mean'], axis=1)
+    x_filtered_cf = np.array(df_x_filtered_cf)
+    y_filtered_cf = y_filtered
 
     return (
         *train_test_split(x, y, random_state=42),
         *train_test_split(x_cf, y_cf, random_state=42),
-        *train_test_split(x_fn, y_fn, random_state=42),
-        *train_test_split(x_fn_cf, y_fn_cf, random_state=42),
+        *train_test_split(x_filtered, y_filtered, random_state=42),
+        *train_test_split(x_filtered_cf, y_filtered_cf, random_state=42),
     )
